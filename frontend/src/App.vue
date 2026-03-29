b529fd73eb7664db934857eb5cdf87ec8651d35c
Braden <braden.kerber@student.adelaide.edu.au>
2026-03-30 09:34:22 +1030

    Backend Laravel 10 set up. Need to confirm the steps to run on a new client.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,25 @@
-<script setup></script>
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import axios from 'axios'
+
+  const message = ref('Waiting for Laravel...')
+
+  // This function talks to the backend
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/data')
+      message.value = response.data.content
+    } catch (error) {
+      message.value = "Laravel error."
+    }
+  }
+
+  onMounted(() => {
+    fetchData()
+  })
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Vue + Laravel Connection</h1>
+  <p>Message from Backend: <strong>{{ message }}</strong></p>
 </template>
-
-<style scoped></style>
