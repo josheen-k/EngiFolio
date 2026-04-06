40c59b19b4ca3fdc84c6d1482d16168446b54dc7
josheen-k <joshhh.kour@gmail.com>
2026-04-06 15:41:17 +0930

    - tried bootstrap for styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,107 +1,115 @@
 <template>
-    <div>
-        <h1>Crud Example</h1>
+  <div class="container w-50 mt-5">
+    <h1 class="text-center display-1">Crud Example</h1>
 
-        <div class="student" v-for="s in students" :key="s.id">
-            <span>{{ s.name }} ({{ s.student_id }}) - {{ s.grade }}</span>
+    <div class="list-group m-5">
+      <div class="list-group-item" v-for="s in students" :key="s.id">
 
-            <div class="student" v-if="!s.editing">
-               <button @click="s.editing=true">Update Student</button>
-               <button @click="deleteStudent(s.id)">Delete student</button>
-            </div>
+        <div class="d-flex justify-content-between align-items-center" v-if="!s.editing">
+          <span>{{ s.name }} ({{ s.student_id }}) - {{ s.grade }}</span>
 
-            <div class="student" v-else>
-               <input v-model="s.name" placeholder="Name"/>
-               <input v-model="s.student_id" placeholder="ID"/>
-               <input v-model="s.grade" placeholder="Grade"/>
-               <button @click="updateStudent(s)">Save</button>
-               <button @click="s.editing=false">Cancel</button>
-            </div>
+          <div class="d-flex gap-2">
+            <button class="btn btn-primary btn-sm" @click="s.editing = true">Update</button>
+            <button class="btn btn-danger btn-sm" @click="deleteStudent(s.id)">Delete</button>
+          </div>
         </div>
 
-        <div class="add">
-            <input v-model="name" placeholder="Name"/>
-            <input v-model="student_id" placeholder="ID"/>
-            <input v-model="grade" placeholder="Grade"/>
-            <button @click="addStudent">Add student</button>
-        </div>
+        <div class="row g-2 mt-2" v-else>
+          <div class="col-3">
+            <input class="form-control" v-model="s.name" placeholder="Name" />
+          </div>
 
+          <div class="col-3">
+            <input class="form-control" v-model="s.student_id" placeholder="ID" />
+          </div>
+
+          <div class="col-3">
+            <input class="form-control" v-model="s.grade" placeholder="Grade" />
+          </div>
+
+          <div class="col-3 d-flex gap-2">
+            <button class="btn btn-success" @click="updateStudent(s)">Save</button>
+            <button class="btn btn-secondary" @click="s.editing = false">Cancel</button>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="card p-3 m-5">
+      <h5 class="mb-3">Add Student</h5>
+
+      <div class="row">
+        <div class="col-3">
+          <input class="form-control" v-model="name" placeholder="Name" />
+        </div>
+
+        <div class="col-3">
+          <input class="form-control" v-model="student_id" placeholder="ID" />
+        </div>
+
+        <div class="col-3">
+          <input class="form-control" v-model="grade" placeholder="Grade" />
+        </div>
+
+        <div class="col-3">
+          <button class="btn btn-success" @click="addStudent">Add student</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import studentService from '@/services/studentService';
 
 export default {
-    data() {
-        return {
-            students: [],
-            name: '',
-            student_id: '',
-            grade: ''
-        };
+  data() {
+    return {
+      students: [],
+      name: '',
+      student_id: '',
+      grade: ''
+    };
+  },
+
+  methods: {
+    load() {
+      studentService.getStudents().then(res => {
+        this.students = res.data.map(s => ({
+          ...s,
+          editing: false
+        }));
+      });
     },
 
-    methods: {
-        load() {
-            studentService.getStudents().then(res => {
-                this.students = res.data.map(s => ({
-                  ...s,
-                  editing: false
-                }));
-            });
-        },
-
-        addStudent() {
-            studentService.createStudent({
-                name: this.name,
-                student_id: this.student_id,
-                grade: this.grade
-            }).then(() => {
-                this.load();
-
-                this.name = '';
-                this.student_id = '';
-                this.grade = '';
-            });
-        },
-
-        updateStudent(s) {
-            studentService.updateStudent(s.id, {
-              name: s.name,
-              student_id: s.student_id,
-              grade: s.grade
-            }).then(() => this.load());
-        },
-
-        deleteStudent(id) {
-            studentService.deleteStudent(id).then(() => this.load());
-        }
-    },
-    mounted() {
+    addStudent() {
+      studentService.createStudent({
+        name: this.name,
+        student_id: this.student_id,
+        grade: this.grade
+      }).then(() => {
         this.load();
+
+        this.name = '';
+        this.student_id = '';
+        this.grade = '';
+      });
+    },
+
+    updateStudent(s) {
+      studentService.updateStudent(s.id, {
+        name: s.name,
+        student_id: s.student_id,
+        grade: s.grade
+      }).then(() => this.load());
+    },
+
+    deleteStudent(id) {
+      studentService.deleteStudent(id).then(() => this.load());
     }
+  },
+  mounted() {
+    this.load();
+  }
 };
 </script>
-
-<style>
-h1 {
-  text-align: center;
-}
-
-.student {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-}
-
-span {
-  margin: 20px;
-}
-
-.add {
-  display: flex;
-  gap: 10px;
-  margin: 20px;
-}
-</style>
