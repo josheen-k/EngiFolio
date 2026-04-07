084d44e9920d3eb871470744838c4310504b8873
josheen-k <joshhh.kour@gmail.com>
2026-04-07 20:43:28 +0930

    - improving file structure and added basic boiler plate template for homepage

---
+++ frontend/src/pages/public/Homepage.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="homepage">
+        <nav class="nav">
+
+            <div class="logo">
+                <span>ENGI</span>
+                <span>FOLIO</span>
+            </div>
+
+            <button>Sign in</button>
+        </nav>
+
+        <section class="main-hero">
+
+            <div class="hero-content">
+                <div class="hero-tag">
+                    <span>A PORTFOLIO PERSONALISED TO TRACK YOUR PROGRESS</span>
+                </div>
+
+                <div class="hero-title">
+                    <h1>The smarter way<br>to navigate your<br><span>ENGINEERING</span>journey</h1>
+                </div>
+
+                <div class="hero-desc">
+                    <p>To meet Engineers Australia accreditation requirements,engineering students must be given opportunities throughout
+                        their program to reflect on their development of the Engineers Australia Stage 1 competencies. With <span>EngiFolio</span> students can
+                        track their professional development, record competency progress, and build a structured portfolio aligned with industry
+                        accreditation standards.
+                    </p>
+
+                </div>
+
+                <div class="hero-actions">
+                    <button>Explore features</button>
+                    <button>See demo</button>
+                </div>
+            </div>
+
+            <div class="hero-image"></div>
+        </section>
+
+        <div class="divider"></div>
+
+        <section class="key-features">
+            <h2>Key Features</h2>
+            <div class="features-box">
+
+                <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+
+                 <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+
+                 <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+
+                 <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+
+                 <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+
+                 <div class="feature">
+                    <img>
+                    <h3>Feature name</h3>
+                    <p>feature desc</p>
+                </div>
+            </div>
+        </section>
+
+        <div class="divider"></div>
+
+        <footer class="footer">
+            <span>Copyright EngiFolio 2026</span>
+
+            <div class="logo">
+                <span>ENGI</span>
+                <span>FOLIO</span>
+            </div>
+
+            <span><u>Privacy Policy</u></span>
+        </footer>
+
+    </div>
+</template>
