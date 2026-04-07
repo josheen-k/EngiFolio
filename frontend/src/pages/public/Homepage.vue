4ef57ca31e1249379e9285fb3ecf3d0216b22076
josheen-k <joshhh.kour@gmail.com>
2026-04-07 21:07:06 +0930

    - playing around with bootstrap styling for this page, might need custom css too if wanna make identical to figma ui

--- frontend/src/pages/public/Homepage.vue
+++ frontend/src/pages/public/Homepage.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="homepage">
-        <nav class="nav">
+    <div class="page">
+        <nav class="navbar px-4 px-md-5 py-3 d-flex justify-content-between align-items-center">
 
             <div class="logo">
-                <span>ENGI</span>
-                <span>FOLIO</span>
+                <span class="logo-engi">ENGI</span>
+                <span class="logo-folio">FOLIO</span>
             </div>
 
-            <button>Sign in</button>
+            <button class="btn btn-dark btn-signin px-4">Sign in</button>
         </nav>
 
-        <section class="main-hero">
+        <section class="container-lg py-5">
 
-            <div class="hero-content">
-                <div class="hero-tag">
+            <div class="row align-items-center g-5">
+                <div class="col-12 col-md-6">
                     <span>A PORTFOLIO PERSONALISED TO TRACK YOUR PROGRESS</span>
                 </div>
 
                 <div class="hero-title">
-                    <h1>The smarter way<br>to navigate your<br><span>ENGINEERING</span>journey</h1>
+                    <h1>The smarter way<br>to navigate your<br><span>ENGINEERING </span>journey</h1>
                 </div>
 
                 <div class="hero-desc">
