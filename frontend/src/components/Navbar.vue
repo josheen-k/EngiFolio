6c4303e46583a9272ca5ac913193076f30dde6c9
josheen-k <joshhh.kour@gmail.com>
2026-04-09 21:28:58 +0930

    -started coding for the student dashboard, starting with a separate nav bar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <div>EngiFOlio</div>
+        <div>
+            <ul>
+                <li>
+                    <a href="../pages/student/Dashboard.vue">Dashboard</a>
+                </li>
+                <li>
+                    <a href="../pages/student/Goals.vue">Goals</a>
+                </li>
+                <li>
+                    <a href="../pages/student/Networking.vue">Networking</a>
+                </li>
+                <li>
+                    <a href="../pages/student/eaCompetency.vue">Competencies</a>
+                </li>
+                <li>
+                    <a href="../pages/student/careerDevelopment.vue">CDL</a>
+                </li>
+            </ul>
+            <div>
+                <img src="" alt="Profile">
+            </div>
+        </div> 
+    </nav>
+</template>
