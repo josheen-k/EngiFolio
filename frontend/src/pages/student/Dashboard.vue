afa03b2f2c6997cb432a971631c4b10bff4ae5c7
josheen-k <joshhh.kour@gmail.com>
2026-04-11 21:26:40 +0930

    - added fields for need more focus table and recent actvity

--- frontend/src/pages/student/Dashboard.vue
+++ frontend/src/pages/student/Dashboard.vue
@@ -14,9 +14,28 @@
     <div class="row g-4">
       <div class="col-12 col-lg-8">
         <h2>Need More Focus On</h2>
+
+        <table class="table">
+          <tbody>
+            <tr v-for="item in focusItems" :key="item.id">
+              <td>{{ item.id }}</td>
+              <td>{{ item.description }}</td>
+              <td>{{ item.entries }}</td>
+              <td>{{ item.level }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
       <div class="col-12 col-lg-4">
         <h2>Quick Links</h2>
+      </div>
+      <div class="col-12 col-lg-4">
+        <h2>Recent Activity</h2>
+        <ul>
+          <li v-for="act in recentAct" :key="act">
+            {{ act }}
+          </li>
+        </ul>
       </div>
     </div>
   </main>
@@ -25,6 +44,50 @@
 </template>
 
 <script setup>
-  import Navbar from '@/components/Navbar.vue'
-  import Footer from '@/components/Footer.vue'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+const focusItems = [
+  {
+    id: '1.2',
+    description: 'Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.',
+    entries: 0,
+    level: 'Emerging'
+  },
+
+  {
+    id: '1.2',
+    description: 'Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.',
+    entries: 0,
+    level: 'Emerging'
+  },
+
+  {
+    id: '1.2',
+    description: 'Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.',
+    entries: 0,
+    level: 'Emerging'
+  },
+
+  {
+    id: '1.2',
+    description: 'Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.',
+    entries: 0,
+    level: 'Emerging'
+  },
+
+  {
+    id: '1.2',
+    description: 'Comprehensive, theory based understanding of the underpinning natural and physical sciences and the engineering fundamentals applicable to the engineering discipline.',
+    entries: 0,
+    level: 'Emerging'
+  },
+]
+
+const recentAct = [
+  '2 Apr 2026: Added reflection for competency 1.2',
+  '1 Apr 2026: Updated goal: Do 3 mini projects',
+  '25 Mar 2026: Deleted goal: Improve teamwork',
+  '22 Mar 2026: Updated profile'
+]
 </script>
