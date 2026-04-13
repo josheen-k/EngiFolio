645616d5f30712b630f7d4a84c29e2c360e291a9
josheen-k <joshhh.kour@gmail.com>
2026-04-13 19:26:18 +0930

    - dynmic title based on tab, side panel looks better

--- frontend/src/pages/student/eaCompetency.vue
+++ frontend/src/pages/student/eaCompetency.vue
@@ -3,13 +3,14 @@
 
   <div class="d-flex gap-4 p-4">
     <aside class="d-flex gap-2 flex-column pt-5">
-      <div class="d-flex align-items-center gap-2 px-3 py-2"
-      :class="{'sidebar-active': currTab===t}" v-for="t in tabs" :key="t" 
-      @click="currTab = t">{{ t }}</div>
+      <div class="d-flex align-items-center gap-2 px-3 py-2 sidebar"
+      :class="{'sidebar-on': currTab===t}" v-for="t in tabs" :key="t"  @click="currTab = t">
+        <span class="dot rounded-circle" :class="currTab===t ? 'dot-on' : ''"></span>{{ t }}
+      </div>
     </aside>
 
     <main>
-      <h1>Current Competencies</h1>
+      <h1>{{currTitle}}</h1>
 
       <section v-for="g in filteredGrps" :key="g.group_id">
         <h2>{{ g.group_name }}</h2><span><b>6</b></span>
@@ -29,8 +30,25 @@ import { ref, computed } from 'vue'
 import Navbar from '@/components/Navbar.vue';
 import Footer from '@/components/Footer.vue';
 
-const currTab = ref('CURRENT')
-const tabs = ['CURRENT', 'DRAFTS', 'FEEDBACK', 'DISCONTINUED']
+// different tabs in side pannel
+const currTab = ref('CURRENT');
+const tabs = ['CURRENT', 'DRAFTS', 'FEEDBACK', 'DISCONTINUED'];
+
+// dynamic titles based on current tab
+const currTitle = computed(()=> {
+  switch (currTab.value) {
+    case 'CURRENT':
+      return 'Current Competencies'
+    case 'DRAFTS':
+      return 'Draft Reflections'
+    case 'FEEDBACK':
+      return 'Feedback Recieved'
+    case 'DISCONTINUED':
+      return 'Discontinued Competencies'
+    default:
+      return 'Competencies'
+  }
+});
 
 // dummy data
 const groups = [
@@ -94,9 +112,32 @@ const groups = [
       }
     ]
   }
-]
+];
 
 const filteredGrps = computed(() => {
   return groups.filter(g=>g.status === currTab.value)
-})
+});
 </script>
+
+<style scoped>
+.sidebar{
+  font-family: 'Maven Pro', sans-serif;
+  font-size: 1.2rem;
+  border-radius: 1.5rem;
+}
+
+.sidebar-on {
+  background: #f3f3f3;
+  color: #222222;
+}
+
+.dot {
+  width: 0.7rem;
+  height: 0.7rem;
+  background: #e0e0e0;
+}
+
+.dot-on {
+  background: #88c2d2;
+}
+</style>
