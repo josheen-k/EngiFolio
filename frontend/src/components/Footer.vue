97ca52a68015148218f07140a5a18a2babd7139c
josheen-k <joshhh.kour@gmail.com>
2026-04-18 23:12:53 +0930

    styled footer to match navbar

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,43 +1,63 @@
 <template>
 
   <footer class="footer">
-    <Divider />
-
-    <div class="footer-content d-flex justify-content-between flex-wrap gap-3 px-4 px-md-5 py-4">
-      <span>©EngiFolio 2026</span>
-      <div class="logo-img"></div>
-      <span><u>Privacy Policy</u></span>
+    <div class="footer-content d-flex justify-content-between flex-wrap">
+      <div class="footer-left ms-4 d-flex flex-column align-items-center">
+        <div class="logo-img"></div>
+        <span>©EngiFolio 2026</span>
+      </div>
+      <span>EngiFolio acknowledges the Kaurna people as the Traditional Owners of the Country<br>
+        where the city of Adelaide is situated today, and pays its respects to Elders past and present.</span>
+      <span class="me-4 footer-right"><u>Privacy Policy</u></span>
     </div>
   </footer>
 
 </template>
 
 <script>
-import Divider from './Divider.vue';
+// import Divider from './Divider.vue';
 
 export default {
-  name: 'Footer',
-  components: {
-    Divider
-  }
+  name: 'Footer'
 };
 </script>
 
 <style scoped>
+.footer {
+  background: #130f4d;
+}
 
 .footer-content {
-  margin-top: -3.5rem;
   font-family: 'Maven Pro', sans-serif;
-  color: #2b2b2b;
+  color: #a7a7a7;
+  align-items: center;
+}
+
+span {
+  text-align: center;
+}
+
+.footer-left span {
+  margin-top: -0.3rem;
+  margin-bottom: 0.5rem;
+}
+
+.footer-right {
+  cursor: pointer;
+  transition: color 0.3s ease;
+}
+
+.footer-right:hover {
+  color: #ffffff;
 }
 
 .logo-img {
-		width: 6.25rem;
-		height: 3.75rem;
-		background: linear-gradient(45deg, #d9bebe, #6b6be4);
-		-webkit-mask: url('@/assets/engiFolio.png') no-repeat center;
-		-webkit-mask-size: contain;
-		mask: url('@/assets/engiFolio.png') no-repeat center;
-		mask-size: contain;
-	}
+  width: 5.5rem;
+  height: 3rem;
+  background: linear-gradient(45deg, #d9bebe, #6b6be4);
+  -webkit-mask: url('@/assets/engiFolio.png') no-repeat center;
+  -webkit-mask-size: contain;
+  mask: url('@/assets/engiFolio.png') no-repeat center;
+  mask-size: contain;
+}
 </style>
