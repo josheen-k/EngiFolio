6264556e640d2b0500699d671fe79a805ce25e69
Braden <braden.kerber@student.adelaide.edu.au>
2026-04-28 22:38:40 +0930

    Fixed database again. Hopefully will update this time.

--- backend/database/migrations/2026_04_08_130858_create_users_table.php
+++ backend/database/migrations/2026_04_08_130858_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
-            $table->foreignId('role_id')->constrained('roles', 'role_id');
             $table->string('username', 9)->unique();
             $table->string('email', 254)->unique();
             $table->string('first_name', 50)->nullable();
