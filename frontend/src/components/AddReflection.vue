bd88686551c2efbd804a44903d5fd6ef4805495c
Braden <braden.kerber@student.adelaide.edu.au>
2026-05-24 11:10:07 +0930

    Fixed bug with draft button name

--- frontend/src/components/AddReflection.vue
+++ frontend/src/components/AddReflection.vue
@@ -144,7 +144,8 @@
         <span class="scroll-txt"><u>Scroll to see all fields</u></span>
 
         <div class="d-flex gap-2">
-          <button class="btn btn-filter" @click="saveAsDraft">Save as draft</button>
+          <!-- Non-breaking space to fix no space between as and draft -->
+          <button class="btn btn-filter" @click="saveAsDraft">Save as&nbsp; draft</button>
           <button class="btn btn-filter" @click="$emit('close')">Cancel</button>
           <button class="btn btn-add" @click="save">Done</button>
         </div>
