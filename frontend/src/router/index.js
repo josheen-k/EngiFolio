1841e3cf068e9ea7b6e0b02c9b7fb39aafbcb624
kimmy <gancaiyuin@gmail.com>
2026-05-25 03:00:05 +0930

    fixed index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,7 +14,6 @@ import Export from '@/pages/student/export.vue'
 import Login from '@/pages/student/login.vue'
 import GoalsPage from '@/pages/student/GoalsPage.vue'
 import CertificationSettings from '@/pages/student/editCerts.vue'
-import CompetencyPage from '@/pages/staff/competencyPage.vue'
 import AdminPage from '@/pages/admin/admin.vue'
 import GoalFeedback from '@/pages/staff/goalFeedback.vue'
 import CDL from '@/pages/student/CDL.vue'
@@ -88,7 +87,7 @@ const routes = [
   },
 
   {
-    path: '/staff/goal-feedback/:id',
+    path: '/staff/goal-feedback',
     name: 'goalFeedback',
     component: GoalFeedback
   },
@@ -113,10 +112,6 @@ const routes = [
     path: '/certification-settings/:id',
     name: 'certificationSettings',
     component: CertificationSettings
-  },
-  {
-    path: '/competency',
-    component: CompetencyPage
   },
   {
     path: '/student/CDL/:id',
